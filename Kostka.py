d82c19c689ef6bafcd886125f0707bef6b055cdc
vojTTek <88628174+vojTTek@users.noreply.github.com>
2021-10-11 06:40:51 +0200

    Add files via upload
    Concept

---
+++ Kostka.py
@@ -0,0 +1,17 @@
+#gra w kulki - Squid games
+
+
+from random import randrange
+print('Losowanie liczby we wskazanym zakresie - wynik losowy')
+print('Ile ścian ma mieć kostka?')
+s=int(input())
+print('Ile rzutów?')
+t=int(input())
+print('Powyżej jakiej wartości sukces?')
+w=int(input())
+wynik=[]
+for i in range(t):
+     wynik.append(randrange(1, s))
+     print('wyniki kolejnych rzutów', wynik)
+     zaliczone =[n for n in wynik if n>=w] #tutaj można wprowadzić modyfikator
+     print('Rzuty powyżej sukcesu:', zaliczone)
